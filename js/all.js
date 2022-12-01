a2c51eb651d3ae1ef51a47308a98c4e9ea98383c
AHMED MOHAMED <95592444+AHMED-MOHAMED320@users.noreply.github.com>
2022-12-01 19:15:01 +0200

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -4,7 +4,7 @@ document.querySelector(".bell").onclick = function(){
 //---------------------------------------
 
 
-let countDownDate = new Date("Nov 30, 2022 23:59:59").getTime();
+let countDownDate = new Date("Dec 2, 2022 23:59:59").getTime();
 // console.log(countDownDate);
 
 let counter = setInterval(() => {
@@ -16,12 +16,12 @@ let counter = setInterval(() => {
 
   // Get Time Units
   // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let dayss = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
   let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
 
-  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".days").innerHTML = dayss < 10 ? `0${dayss}` : dayss;
   document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
   document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
   document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
